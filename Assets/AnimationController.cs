249f92050ef7aaa3a40509aa3363e007ba8ae9ee
sidechara <creativenamegoeshere0@gmail.com>
2023-11-11 14:34:26 -0800

    outline for enemy controller
    prob doesn't work

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,20 @@
+/*using UnityEngine;
+
+namespace Platformer.Mechanics {
+    public class AnimationController {
+        public float maxSpeed = 7;
+        public float jumpTakeOffSpeed = 7;
+
+        public Vector2 move;
+        public bool jump;
+        public bool stopJump;
+        SpriteRenderer spriteRenderer;
+        Animator animator;
+        ///PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        protected virtual void Awake() {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+        }
+    }
+}*/
