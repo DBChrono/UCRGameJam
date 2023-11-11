e51dda66014730ac67a1e0edecff2b9ebcc6c197
sidechara <creativenamegoeshere0@gmail.com>
2023-11-11 15:04:34 -0800

    enemymovements.cs

---
+++ Assets/EnemyMovements.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Platformer.Mechanics {
+    public class EnemyMovements : MonoBehaviour
+    {
+        public PatrolPath path;
+        internal PatrolPath.Mover mover;
+        /// <summary>
+        internal AnimationController control;
+        /// </summary>
+        internal Collider2D _collider;
+        SpriteRenderer spriteRenderer;
+        public Bounds bounds => _collider.bounds;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _collider = GetComponent<Collider2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (path != null) {
+                if (mover == null) mover = path.CreateMover(7*0.5f);
+            }
+        }
+    }
+}
