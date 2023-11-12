260c6f76363a293d4d09deb63e37d589ad5a1464
Kirbycraft1 <ksl2005k@outlook.com>
2023-11-11 16:59:13 -0800

    Fixed Enter stuff

--- Assets/ChangeTitleScreen.cs
+++ Assets/ChangeTitleScreen.cs
@@ -6,21 +6,17 @@ using UnityEngine.UI;
 
 public class ChangeIntroScreen : MonoBehaviour
 {
-    int counter = 0;
     // Start is called before the first frame update
     void Start()
     {
-        counter = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space)){
-            counter++;
-        }
-        if(counter > 0){
-           SceneManager.LoadScene("BackgroundInfo");
+        if(Input.GetKey(KeyCode.Return)){
+            SceneManager.LoadScene("BackgroundInfo");
         }
           
     }
