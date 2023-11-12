9772031a7beef003d3413652acd5f98bcb166486
sidechara <creativenamegoeshere0@gmail.com>
2023-11-11 17:15:01 -0800

    enemy movement (for real)

--- Assets/Scripts/EnemyMovements.cs
+++ Assets/Scripts/EnemyMovements.cs
@@ -15,6 +15,13 @@ namespace Platformer.Mechanics {
         SpriteRenderer spriteRenderer;
         public Bounds bounds => _collider.bounds;
         // Start is called before the first frame update
+
+        [SerializeField] private Rigidbody2D rb;
+        public float speed = 10f;
+        private bool facingRight = true;
+
+        private double c = 0;
+
         void Start()
         {
             control = GetComponent<AnimationController>();
@@ -26,10 +33,28 @@ namespace Platformer.Mechanics {
         // Update is called once per frame
         void Update()
         {
+            if (facingRight == true) {
+                rb.velocity = new Vector2(2, 0);
+            } else {
+                rb.velocity = new Vector2(-2, 0);
+            }
+            c+=0.01;
+            if (c >= 6) {
+                c = 0;
+                if (facingRight == true) {
+                    facingRight = false;
+                } else {
+                    facingRight = true;
+                }
+            }
+
+
             if (path != null) {
                 if (mover == null) mover = path.CreateMover(7*0.5f);
                 control.move.x = Mathf.Clamp(mover.Position.x - transform.position.x, -1, 1);
             }
         }
+
+
     }
 }
